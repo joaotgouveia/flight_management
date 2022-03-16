e8a356d33f48b670395b98bfb0c193c5abcee9dd
Joao Talone Gouveia <jjtucki4@gmail.com>
2022-03-16 16:08:56 +0000

    Define values and create structs

--- project.c
+++ project.c
@@ -1,7 +1,39 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
+/* Airport related values */
+#define MAXAIRPORTS 40
+#define IDSIZEAP 3
+#define COUNTRYSIZE 30
+#define CITYSIZE 50
+/* Flight related values */
+#define MAXFLIGHTS 30000
+#define IDSIZEFL 6
+/* Time and date related values */
+#define TIMESIZE 5
+#define DATESIZE 10
+
+/* Prototyping structs */
+struct airport;
+struct flight;
 
 int main () {
 	
 	return 0;
 }
+
+struct airport {
+	char cId[IDSIZEAP];
+	char cCountry[COUNTRYSIZE];
+	char cCity[CITYSIZE];
+};
+
+struct flight {
+	char cId[IDSIZEFL];
+	struct airport aDeparture;
+	struct airport aArrival;
+	int iDuration;
+	int iMaxPassengers;
+
+};
+
