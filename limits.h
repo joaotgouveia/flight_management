bd032d1976b7714797e8de2aa8af531ee25dd73b
Joao Talone Gouveia <jjtucki4@gmail.com>
2022-03-20 12:54:36 +0000

    Create limits.h

---
+++ limits.h
@@ -0,0 +1,14 @@
+/* Airport related values */
+#define MAXAIRPORTS 40
+#define IDSIZEAP 4
+#define COUNTRYSIZE 31
+#define CITYSIZE 51
+/* Flight related values */
+#define MAXFLIGHTS 30000
+#define IDSIZEFL 7
+/* Time and date related values */
+#define TIMESIZE 6
+#define DATESIZE 11
+/* Arg related values */
+#define ARGSIZE MAXAIRPORTS*(IDSIZEAP+1)+3
+#define ARGSTART 2
