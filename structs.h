a300defdfe5a2294328c06d9e8d9f70f13faca96
Joao Talone Gouveia <jjtucki4@gmail.com>
2022-03-24 12:04:41 +0000

    Remove unnecessary words

--- structs.h
+++ structs.h
@@ -1,23 +1,28 @@
 /* Defining structs */
-typedef struct date{
+typedef struct {
 	char day[DAY];
 	char month[MONTH];
 	char year[YEAR];
 } date;
 
-typedef struct airport {
+typedef struct {
+	char hours[HOURS];
+	char mins[MINS];
+} time;
+
+typedef struct {
 	char id[IDAP];
 	char country[COUNTRY];
 	char city[CITY];
 	int departures;
 } airport;
 
-typedef struct flight {
+typedef struct {
 	char id[IDFL];
 	char departure[IDAP];
 	char arrival[IDAP];
 	date date;
-	char time[TIME];
-	int duration;
-	int maxPassengers;
+	time time;
+	time duration;
+	int capacity;
 } flight;
