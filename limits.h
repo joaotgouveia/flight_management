93e4b6289e6be4b45c88b96ea6be5be9c7b0cc29
Joao Talone Gouveia <jjtucki4@gmail.com>
2022-03-24 12:03:59 +0000

    Update limits.h

--- limits.h
+++ limits.h
@@ -6,16 +6,23 @@
 /* Flight related values */
 #define MAXFLIGHTS 30000
 #define IDFL 7
+#define CAPACITY 4
 /* Time and date related values */
+#define HOURS 3
+#define MINS 3
 #define TIME 6
-#define DATE 11
 #define DAY 3
 #define MONTH 3
 #define YEAR 5
-#define DATESTART 1+IDAP*2
+#define DATE 11
+#define DATESTART IDAP*2
+#define CATDATETIME DATE-3+TIME-1
 /* Arg related values */
 #define ARGSIZE MAXAIRPORTS*(IDAP+1)+3
 #define ARGSTART 2
 /* Boolean values */
 #define TRUE 1
 #define FALSE 0
+#define NOTFOUND -1
+/* Data structures */
+#define TUPLE 2
